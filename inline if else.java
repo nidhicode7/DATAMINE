b13f1b22e47d6ff468e55efce666da4fb12ac3bb
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-09-05 18:27:52 +0530

    Create inline if else.java
    Usage of ternary operators

---
+++ inline if else.java
@@ -0,0 +1,7 @@
+class TernaryExample {
+    public static void main(String[] args) {
+        int age = 20;
+        String result = (age >= 18) ? "Adult" : "Minor";
+        System.out.println(result);
+    }
+}
